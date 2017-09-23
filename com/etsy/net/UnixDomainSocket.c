f66e836914faa76af32c5e3950ba4e815a3f4f38
mayunlei <yunleisjtu+github@gmail.com>
2017-09-23 14:03:49 +0800

    remove useless error

--- com/etsy/net/UnixDomainSocket.c
+++ com/etsy/net/UnixDomainSocket.c
@@ -139,7 +139,6 @@ Java_com_etsy_net_UnixDomainSocket_nativeOpen(JNIEnv * jEnv,
     ASSERTNOERR(s == -1, "nativeOpen: socket");
     if (connect(s, (struct sockaddr *)&sa, salen) == -1) {
        (*jEnv)->ReleaseStringUTFChars(jEnv, jSocketFile, socketFile);
-	perror("nativeOpen: connect");
 	int close_ = close(s);
 	ASSERTNOERR(close_ == -1, "nativeOpen: close connect error socket");
 	return -1;
